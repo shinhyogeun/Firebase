2a4045d9f57a7da803c38a7c143a65103118f667
shinhyogeun <tls1gy2rms3@naver.com>
2020-09-19 00:28:23 +0900

    small start in Firebase

---
+++ IntegrateFIrebase/IntegrateFIrebase/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IntegrateFIrebase
+//
+//  Created by 신효근 on 2020/09/17.
+//  Copyright © 2020 신효근. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
