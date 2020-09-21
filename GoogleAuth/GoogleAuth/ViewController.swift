8a9c9194196a7a2ac0266f6c30f409e4c563872c
shinhyogeun <tls1gy2rms3@naver.com>
2020-09-21 16:34:15 +0900

    GIDSignInDelegate and Firebase

---
+++ GoogleAuth/GoogleAuth/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  GoogleAuth
+//
+//  Created by 신효근 on 2020/09/19.
+//
+
+import UIKit
+import Firebase
+import GoogleSignIn
+
+class ViewController: UIViewController{
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        GIDSignIn.sharedInstance()?.presentingViewController = self
+        
+        handleGoogleSingIn()
+    }
+    
+    private func handleGoogleSingIn(){
+        let loginButton = GIDSignInButton()
+        view.addSubview(loginButton)
+        loginButton.frame = CGRect(x: 16, y: 116+16, width: view.frame.width-32, height: 50)
+    }
+
+}
+
