ec78a52863bcf7f6e8b1663de258e0501f1a4a8b
shinhyogeun <tls1gy2rms3@naver.com>
2020-09-26 14:44:58 +0900

    this update is about how to use read in firebase

--- Database2/Database2/ViewController.swift
+++ Database2/Database2/ViewController.swift
@@ -30,25 +30,75 @@ class ViewController: UIViewController, GIDSignInUIDelegate {
     }
     
     @IBAction func create(with:UIStoryboardSegue){
+        //우리가 데이터를 데이터 베이스에 저장할 위치 만들기위해 키를 생성한다. 그 키자리에 넣을거다.(끝없이 생길 수 있다면! autoId가 필요하다)
         guard let key =
-                ref.child("user").child(Auth.auth().currentUser!.uid).child("post")
-                .childByAutoId().key else{return}
-        
+                ref.child("users")
+                .childByAutoId().key else{
+                    return
+                }
+        // post는 저장할 데이터의 구조이다.
         let post = ["index" : String(self.index),
                     "uid" : Auth.auth().currentUser?.uid,
                     "author" : Auth.auth().currentUser?.email,
                     "title" : "My Dog",
                     "body" : "Dummy Blog Post"]
-        
-        let childUpdate = ["/users2/\(Auth.auth().currentUser!.uid)/posts/\(key)" : post]
+        //[위치:무엇을]
+        let childUpdate = ["/users/\(Auth.auth().currentUser!.uid)/posts3/\(key)" : post,
+                           "/users/posts2/\(key)" : post]
+        //저장해라!
         ref.updateChildValues(childUpdate)
         index += 1
     }
     
     @IBAction func read(with:UIStoryboardSegue){
+        let postRef : DatabaseQuery! = ref.child("users").child(Auth.auth().currentUser!.uid).child("posts").queryOrdered(byChild: "/index")
+        postRef.observeSingleEvent(of: DataEventType.value) { (snapshot, key) in
+            let group = DispatchGroup()
+            let children : NSEnumerator = snapshot.children
+            group.enter()
+            for (child) in children {
+                let childSnapShot = child as? DataSnapshot
+                let childPost : NSDictionary = (childSnapShot?.value as? NSDictionary)!
+                print("CHILD KEY: [\(childSnapShot?.key ?? "")]")
+                for (key, value) in childPost{
+                    print("\(key) : \(value) \n")
+                }
+            }
+            group.leave()
+            group.notify(queue: .main){
+            }
+        }
         
     }
     @IBAction func update(with:UIStoryboardSegue){
+        let postRef : DatabaseReference! = ref.child("users").child(Auth.auth().currentUser!.uid).child("posts")
+        postRef.observeSingleEvent(of: DataEventType.value) { (snapshot, key) in
+            let group = DispatchGroup()
+            let childCount : NSEnumerator = snapshot.children
+            var count : Int = 0
+            group.enter()
+            for (_) in childCount{
+                count += 1
+            }
+            
+            let children : NSEnumerator = snapshot.children
+            for (child) in children{
+                count -= 1
+                let childSnapShot = child as? DataSnapshot
+                postRef.child(childSnapShot!.key).child("index").setValue(String(count))
+            }
+            
+            group.leave()
+            group.notify(queue: .main){
+                
+            }
+            
+            
+            
+            
+            
+            
+        }
         
     }
     @IBAction func delete(with:UIStoryboardSegue){
